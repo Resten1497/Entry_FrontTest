e05638f58844ae1d8771c517b201e92b4b3aed1a
Resten1497 <saup1965@gmail.com>
2022-07-26 01:27:49 +0900

    MODIFY: Button Component

--- src/components/NextButton/index.jsx
+++ src/components/NextButton/index.jsx
@@ -1,17 +1,12 @@
 import "./NextBtn.css";
 import Next from "../../assets/images/NextPage.png";
-import { Link } from "react-router-dom";
 
 function NextBtn(prop) {
-  console.log(prop.link);
-
   return (
-    <Link to={prop.link} style={{ textDecoration: "none" }}>
-      <button type="submit" className="NextBtn">
-        <span className="btnTitle">다음</span>
-        <img src={Next} className="BtnImg" alt="Next" />
-      </button>
-    </Link>
+    <button type="submit" className="NextBtn" onClick={prop.onclick}>
+      <span>다음</span>
+      <img src={Next} className="BtnImg" alt="Next" />
+    </button>
   );
 }
 
--- src/container/IntroduceContainer/index.jsx
+++ src/container/IntroduceContainer/index.jsx
@@ -1,20 +1,23 @@
 import logo from "../../assets/images/schoolLogo.png";
 import "./IntroduceContainer.css";
 import NextBtn from "../../components/NextButton";
-
+import { useNavigate } from "react-router-dom";
+import { useCallback } from "react";
 function IntroduceContainer() {
+  const navigate = useNavigate();
+  const handleLinkOnClick = useCallback(() => navigate("/regist"), [navigate]);
   return (
     <div className="container">
       <div>
         <img src={logo} className="Logo" alt="schoolLogo" />
         <p className="MainText">
           서울디지텍고등학교에
-            <br />
-            오신것을 환영합니다.
-          </p>
+          <br />
+          오신것을 환영합니다.
+        </p>
         <p className="SubText">신분확인을 위해 몇가지를 작성해주세요!!</p>
       </div>
-      <NextBtn link={"/regist"} className="NextBtn" />
+      <NextBtn onclick={handleLinkOnClick} className="NextBtn" />
     </div>
   );
 }
