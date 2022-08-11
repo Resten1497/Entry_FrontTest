9357100cd7d02c482eff90df26bbcbc78cb7b3da
Resten1497 <saup1965@gmail.com>
2022-08-11 22:34:27 +0900

    ADD Alert Lib

--- src/container/CameraContainer/index.jsx
+++ src/container/CameraContainer/index.jsx
@@ -4,24 +4,46 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { useMutation } from "@tanstack/react-query";
 import sendCardData from "../../api/sendCardData";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const CameraContainer = () => {
   const [cardId, setCardId] = useState(null);
   const navigate = useNavigate();
   const handleLinkOnClick = (cardId) => navigate("/regist", { state: cardId });
   let ref = useRef();
+  const toastId = useRef(null);
+  const notify = () =>
+    (toastId.current = toast("인식중입니다.", {
+      type: toast.loading,
+      autoClose: 1000,
+      delay: 100,
+    }));
   const { mutate, isLoading, isError, error, isSuccess } = useMutation(
     sendCardData,
     {
       onSuccess: (data) => {
-        console.log(data);
-        handleLinkOnClick(cardId);
-        ref.current.stopCamera();
+        toast.update(toastId.current, {
+          render: "완료되었습니다.",
+          type: toast.success,
+          onClose: () => {
+            handleLinkOnClick(cardId);
+            ref.current.stopCamera();
+          },
+        });
+      },
+      onError: (error) => {
+        toast.update(toastId.current, {
+          render: "인증되지 않은 카드입니다.",
+          type: toast.error,
+          autoClose: 1000,
+        });
       },
     }
   );
   useEffect(() => {
     if (cardId) {
+      notify();
       mutate({ cardId: cardId });
       setCardId(null);
     }
@@ -43,8 +65,9 @@ const CameraContainer = () => {
           onError={(err) => console.log(err)}
         />
         <Title>QR 코드를 화면에 보여주세요</Title>
-        {isLoading ? <WarinningText>요청중</WarinningText> : null}
-        {isError ? <WarinningText>잘못된 QR코드입니다.</WarinningText> : null}
+        {/* {isLoading ? <WarinningText>요청중</WarinningText> : null}
+        {isError ? <WarinningText>잘못된 QR코드입니다.</WarinningText> : null} */}
+        <ToastContainer position="bottom-right" />
       </Wrap>
     </>
   );
--- src/container/InputContainer/index.jsx
+++ src/container/InputContainer/index.jsx
@@ -1,11 +1,15 @@
 import NextBtn from "../../components/buttonComponent/index";
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useLocation, useNavigate } from "react-router-dom";
 import sendVisitorData from "../../api/sendVisitorData";
 import styled from "styled-components";
 
 function InputContainer() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   const location = useLocation();
   console.log("state", location.state);
   const { cardId } = location.state;
@@ -32,126 +36,127 @@ function InputContainer() {
   return (
     <Container>
       <Content>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <Lable>
-          <Title>이름</Title>
-          <StyledInput
-            type="text"
-            name="visitorName"
-            className="info"
-            id="visitorName"
-            placeholder="홍길동 "
-            autoComplete="off"
-            {...register("visitorName", {
-              required: true,
-            })}
-          />
-          <Error>
-            {errors.visitorName?.type === "required" && "이름을 입력해주세요!"}
-          </Error>
-        </Lable>
-        <Lable>
-          <Title>전화번호</Title>
-          <StyledInput
-            type="text"
-            name="visitorPhoneNumber"
-            className="info"
-            id="visitorPhoneNumber"
-            placeholder="010-8888-8888"
-            autoComplete="off"
-            {...register("visitorPhoneNumber", {
-              required: true,
-              // pattern: /^01([0|1|6|7|8|9])-([0-9]{3,4})-([0-9]{4})$/,
-              pattern: {
-                value: /^01([0|1|6|7|8|9])-([0-9]{3,4})-([0-9]{4})$/,
-                message: "전화번호 입력을 다시 확인해주세요",
-              },
-            })}
-          />
-          <Error>
-            {errors.visitorPhoneNumber?.type === "required" &&
-              "전화번호를 입력해주세요!"}
-            {errors.visitorPhoneNumber?.message}
-          </Error>
-        </Lable>
-        <Lable>
-          <Title>소속</Title>
-          <StyledInput
-            type="text"
-            name="visitorDivision"
-            className="info"
-            id="visitorDivision"
-            placeholder="서울디지텍고등학교"
-            autoComplete="off"
-            {...register("visitorDivision", {
-              required: true,
-            })}
-          />
-          <Error>
-            {errors.visitorDivision?.type === "required" &&
-              "소속을 입력해주세요!"}
-          </Error>
-        </Lable>
-        <Lable>
-          <Title>방문사유</Title>
-          <StyledInput
-            type="text"
-            name="visitorReason"
-            className="info"
-            id="visitorReason"
-            placeholder="강의진행"
-            autoComplete="off"
-            {...register("visitorReason", {
-              required: true,
-            })}
-          />
-          <Error>
-            {errors.visitorReason?.type === "required" &&
-              "방문사유를 입력해주세요!"}
-          </Error>
-        </Lable>
-        <Lable>
-          <Title>체온</Title>
-          <StyledInput
-            type="text"
-            name="temperature"
-            className="info"
-            id="temperature"
-            placeholder="36.5"
-            autoComplete="off"
-            {...register("temperature", {
-              required: true,
-            })}
-          />
-          <Error>
-            {errors.temperature?.type === "required" &&
-              "현재 온도를 입력해주세요!"}
-          </Error>
-        </Lable>
-        <Lable>
-          <PrivacyLabal>
-            <CheckBox
-              type="checkbox"
-              autoFocus
-              name="privacyLabal"
-              id="privacyLabal"
-              className="checkBox"
-              {...register("privacyLabal", {
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Lable>
+            <Title>이름</Title>
+            <StyledInput
+              type="text"
+              name="visitorName"
+              className="info"
+              id="visitorName"
+              placeholder="홍길동 "
+              autoComplete="off"
+              {...register("visitorName", {
                 required: true,
               })}
             />
-            <Privacy>개인정보수집에 동의합니다.</Privacy>
-          </PrivacyLabal>
-          <Error>
-            {errors.privacyLabal?.type === "required" &&
-              "개인정보수집에 동의해주세요"}
-          </Error>
-        </Lable>
-        <Btn>
-           <NextBtn onClick={null} onKeyDown={handleKeyDown} />
-        </Btn>
-        <input type="hidden" value={cardId} name="cardId" />
-      </Form>
+            <Error>
+              {errors.visitorName?.type === "required" &&
+                "이름을 입력해주세요!"}
+            </Error>
+          </Lable>
+          <Lable>
+            <Title>전화번호</Title>
+            <StyledInput
+              type="text"
+              name="visitorPhoneNumber"
+              className="info"
+              id="visitorPhoneNumber"
+              placeholder="010-8888-8888"
+              autoComplete="off"
+              {...register("visitorPhoneNumber", {
+                required: true,
+                // pattern: /^01([0|1|6|7|8|9])-([0-9]{3,4})-([0-9]{4})$/,
+                pattern: {
+                  value: /^01([0|1|6|7|8|9])-([0-9]{3,4})-([0-9]{4})$/,
+                  message: "전화번호 입력을 다시 확인해주세요",
+                },
+              })}
+            />
+            <Error>
+              {errors.visitorPhoneNumber?.type === "required" &&
+                "전화번호를 입력해주세요!"}
+              {errors.visitorPhoneNumber?.message}
+            </Error>
+          </Lable>
+          <Lable>
+            <Title>소속</Title>
+            <StyledInput
+              type="text"
+              name="visitorDivision"
+              className="info"
+              id="visitorDivision"
+              placeholder="서울디지텍고등학교"
+              autoComplete="off"
+              {...register("visitorDivision", {
+                required: true,
+              })}
+            />
+            <Error>
+              {errors.visitorDivision?.type === "required" &&
+                "소속을 입력해주세요!"}
+            </Error>
+          </Lable>
+          <Lable>
+            <Title>방문사유</Title>
+            <StyledInput
+              type="text"
+              name="visitorReason"
+              className="info"
+              id="visitorReason"
+              placeholder="강의진행"
+              autoComplete="off"
+              {...register("visitorReason", {
+                required: true,
+              })}
+            />
+            <Error>
+              {errors.visitorReason?.type === "required" &&
+                "방문사유를 입력해주세요!"}
+            </Error>
+          </Lable>
+          <Lable>
+            <Title>체온</Title>
+            <StyledInput
+              type="text"
+              name="temperature"
+              className="info"
+              id="temperature"
+              placeholder="36.5"
+              autoComplete="off"
+              {...register("temperature", {
+                required: true,
+              })}
+            />
+            <Error>
+              {errors.temperature?.type === "required" &&
+                "현재 온도를 입력해주세요!"}
+            </Error>
+          </Lable>
+          <Lable>
+            <PrivacyLabal>
+              <CheckBox
+                type="checkbox"
+                autoFocus
+                name="privacyLabal"
+                id="privacyLabal"
+                className="checkBox"
+                {...register("privacyLabal", {
+                  required: true,
+                })}
+              />
+              <Privacy>개인정보수집에 동의합니다.</Privacy>
+            </PrivacyLabal>
+            <Error>
+              {errors.privacyLabal?.type === "required" &&
+                "개인정보수집에 동의해주세요"}
+            </Error>
+          </Lable>
+          <Btn>
+            <NextBtn onClick={null} onKeyDown={handleKeyDown} />
+          </Btn>
+          <input type="hidden" value={cardId} name="cardId" />
+        </Form>
       </Content>
     </Container>
   );
