61c2d69a2ebb2605b2b4e9fc517ddc5decafe8d5
Resten1497 <saup1965@gmail.com>
2022-08-12 23:14:47 +0900

    UPDATE : ADMIN PAGE

--- src/container/InquiryContainer/index.jsx
+++ src/container/InquiryContainer/index.jsx
@@ -5,21 +5,23 @@ import {
 } from "@tanstack/react-query";
 import React from "react";
 import axios from "axios";
-import styled from 'styled-components';
-import Calendar from '../../assets/images/Calendar.png';
+import styled from "styled-components";
+import Calendar from "../../assets/images/Calendar.png";
 
 function InquiryContainer() {
   const day = new Date();
   const year = String(day.getFullYear());
-  let first = String(day.getMonth() + 1).split('');
+  let first = String(day.getMonth() + 1).split("");
   if (first.length === 1) {
-    first.unshift('0')
+    first.unshift("0");
   }
-  const month = [first].join().replace(/,/g, '');
-  const dateValue = year + '-' + month;
+  const month = [first].join().replace(/,/g, "");
+  const dateValue = year + "-" + month;
+
   return (
     <Container>
       <Content>
+<<<<<<< Updated upstream
         <Day type="month" name="day" defaultValue={dateValue} />
         <Table>
           <thead>
@@ -46,6 +48,32 @@ function InquiryContainer() {
                 <GetData />
             </tbody>
           </Table>
+=======
+        <Day
+          type="month"
+          name="day"
+          defaultValue={dateValue}
+          onChange={(e) => {
+            console.log(e.target.value);
+          }}
+        />
+        <HeadTable>
+          <ThRow>
+            <HeadDataNumber>번호</HeadDataNumber>
+            <HeadDataDate>날짜</HeadDataDate>
+            <HeadDataName>이름</HeadDataName>
+            <HeadDataPhone>전화번호</HeadDataPhone>
+            <HeadDataReason>방문목적</HeadDataReason>
+            <HeadDataInTime>입실시간</HeadDataInTime>
+            <HeadDataOutTime>퇴실시간</HeadDataOutTime>
+          </ThRow>
+        </HeadTable>
+        <Over>
+          <BodyTable>
+            <GetData />
+          </BodyTable>
+        </Over>
+>>>>>>> Stashed changes
       </Content>
     </Container>
   );
@@ -63,11 +91,15 @@ function GetData() {
     return (
       <>
         {data.map((item, index) => {
-          console.log(item);
           return (
             <>
+<<<<<<< Updated upstream
               <Row>
                 <Number>{index+1}</Number>
+=======
+              <TbRow>
+                <Number>{index + 1}</Number>
+>>>>>>> Stashed changes
                 <DateTime>07/18</DateTime>
                 <Name>{item.visitorName}</Name>
                 <Phone>{item.visitorPhoneNumber}</Phone>
@@ -96,20 +128,21 @@ const Content = styled.div`
 `;
 
 const Day = styled.input`
-    margin-bottom: 50px;
-    width: 400px;
-    font-style: normal;
-    font-weight: 700;
-    font-size: 3.5rem;
-    border: none;
-    &:focus{
-      outline: none;
-    }
-    &::-webkit-calendar-picker-indicator {
-      background: url(${Calendar});
-    }
+  margin-bottom: 50px;
+  width: 400px;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 3.5rem;
+  border: none;
+  &:focus {
+    outline: none;
+  }
+  &::-webkit-calendar-picker-indicator {
+    background: url(${Calendar});
+  }
 `;
 
+<<<<<<< Updated upstream
 const Table = styled.table`
     width: 1400px;
     height: 60px;
@@ -131,62 +164,168 @@ const Row = styled.tr`
     line-height: 30px;
     border: none;
     text-align: center;
+=======
+const HeadTable = styled.table`
+  width: 1400px;
+  height: 60px;
+  border-collapse: collapse;
+`;
+const ThRow = styled.tr`
+  height: 60px;
+  border: none;
+  text-align: center;
+`;
+
+const HeadDataNumber = styled.td`
+  width: 150px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 22px;
+  text-align: center;
+`;
+
+const HeadDataDate = styled.td`
+  width: 200px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 22px;
+  text-align: center;
+`;
+
+const HeadDataName = styled.td`
+  width: 200px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 22px;
+  text-align: center;
+`;
+
+const HeadDataPhone = styled.td`
+  width: auto;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 22px;
+  text-align: center;
+`;
+
+const HeadDataReason = styled.td`
+  width: 200px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 22px;
+  text-align: center;
+`;
+
+const HeadDataInTime = styled.td`
+  width: 200px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 22px;
+  text-align: center;
+`;
+
+const HeadDataOutTime = styled.td`
+  width: 200px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 22px;
+  text-align: center;
+`;
+
+const Over = styled.div`
+  width: 1400px;
+  height: 480px;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    width: 4px;
+  }
+  &::-webkit-scrollbar-thumb {
+    width: 4px;
+    border-radius: 5px;
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+  &::-webkit-scrollbar-track {
+    background-color: initial;
+  }
+`;
+
+const BodyTable = styled.table`
+  width: 1400px;
+  height: 480px;
+`;
+
+const TbRow = styled.tr`
+  width: 1440px;
+  line-height: 30px;
+  border: none;
+  text-align: center;
+>>>>>>> Stashed changes
 `;
 
 const Number = styled.td`
-    width: 150px;
-    line-height: 30px;
-    text-align: center;
-    font-style: normal;
-    font-weight: 400;
-    font-size: 25px;
+  width: 150px;
+  line-height: 30px;
+  text-align: center;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
 `;
 
 const DateTime = styled.td`
-    width: 200px;
-    line-height: 30px;
-    text-align: center;
-    font-style: normal;
-    font-weight: 400;
-    font-size: 25px;
+  width: 200px;
+  line-height: 30px;
+  text-align: center;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
 `;
 const Name = styled.td`
-    width: 200px;
-    line-height: 30px;
-    text-align: center;
-    font-style: normal;
-    font-weight: 400;
-    font-size: 25px;
+  width: 200px;
+  line-height: 30px;
+  text-align: center;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
 `;
 const Phone = styled.td`
+<<<<<<< Updated upstream
     width: 250px;
     line-height: 30px;
     text-align: center;
     font-style: normal;
     font-weight: 400;
     font-size: 25px;
+=======
+  width: auto;
+  line-height: 30px;
+  text-align: center;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+>>>>>>> Stashed changes
 `;
 const Reason = styled.td`
-    width: 200px;
-    line-height: 30px;
-    text-align: center;
-    font-style: normal;
-    font-weight: 400;
-    font-size: 25px;
+  width: 200px;
+  line-height: 30px;
+  text-align: center;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
 `;
 const InTime = styled.td`
-    width: 200px;
-    line-height: 30px;
-    text-align: center;
-    font-style: normal;
-    font-weight: 400;
-    font-size: 25px;
+  width: 200px;
+  line-height: 30px;
+  text-align: center;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
 `;
 const OutTime = styled.td`
-    width: 200px;
-    line-height: 30px;
-    text-align: center;
-    font-style: normal;
-    font-weight: 400;
-    font-size: 26px;
+  width: 200px;
+  line-height: 30px;
+  text-align: center;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 26px;
 `;
