2627e6c0e3509cb2c8b69689e8d9491b2727fbd5
Resten1497 <saup1965@gmail.com>
2022-08-15 23:39:53 +0900

    ADD async

--- src/api/sendVisitorData.js
+++ src/api/sendVisitorData.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import qs from "qs";
 
-export default function sendVisitorData(data) {
-    return axios.post(process.env.REACT_APP_sendVisitorData, data);
+export default async function sendVisitorData(data) {
+  return await axios.post(process.env.REACT_APP_sendVisitorData, data);
 }
