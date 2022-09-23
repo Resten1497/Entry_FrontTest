b655fd73e9ace16e62606e7f7b3383db0b738bab
gywns022727 <lifford050227a@gmail.com>
2022-09-23 15:35:57 +0900

    api

--- src/api/sendVisitorData.js
+++ src/api/sendVisitorData.js
@@ -2,5 +2,5 @@ import axios from "axios";
 import qs from "qs";
 
 export default async function sendVisitorData(data) {
-  return await axios.post(process.env.REACT_APP_sendVisitorData, data);
+    return await axios.post(process.env.REACT_APP_sendVisitorData, data);
 }
